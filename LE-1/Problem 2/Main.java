88a67f5d9b17292f508a32f5e5508bf3fbadcd0d
221B376 <221B376@juet.co.in>
2025-01-16 12:43:43 +0530

    LE-1 Problems

---
+++ LE-1/Problem 2/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Mother mother = new Mother();
+        mother.show();  
+ 
+        Mother child = new Child();  
+        child.show();  
+ 
+        Child directChild = new Child();
+        directChild.show();  
+    }
+}
